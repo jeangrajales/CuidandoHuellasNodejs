ca5300132c010f2956dc39865952774ca26fbc8c
jeangrajales <jeancg2004@gmail.com>
2025-04-22 15:55:39 -0500

    Se renderizo la pagina principal

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 const exp = require("express")
-require('dotenv').config()
+const modeloProducto = require('./src/models/producto.model')
 
 const app = exp();
+
+
+app.set('view engine', 'ejs'); // Se Configura EJS como motor de plantillas
+
+// Asegúrar de que las vistas se busquen en la carpeta correcta
+app.set('views', __dirname + '/src/views');
+app.use(exp.static(__dirname + '/src/public'));
+
+
+app.get('/', (req, res) => {
+  res.render('pages/pagina_principal'); // Renderiza las plantillas desde la carpeta 'views'
+});
+
+app.get('/productos', async(req, res)=>{
+    let listaProducto = await modeloProducto.find({});
+    console.log(listaProducto)
+    if (listaProducto){
+        res.json(listaProducto);
+    }else{
+        res.json({"Error": "Hubo un error"})
+    }
+
+})
+
+app.post('/productos', async(req,res)=>{
+    const nuevoProducto = {
+        nombre: req.body.nombre,
+        precio: req.body.precio,
+    };
+
+    let Insercion = await modeloProducto.create(nuevoProducto);
+    if (Insercion)
+        res.status(200).json({"Mensaje": "Registo Exitoso"})
+    else
+        res.status(404).json({"Mensaje": "Se presento un Error"})
+})
+
 
 //callback
 app.listen(7777,()=>{
--- src/config/connection.js
+++ src/config/connection.js
@@ -8,3 +8,16 @@ const URI = `mongodb+srv://${process.env.USUARIOBD}:${process.env.PASSBD}@adso28
 mongoose.connect(URI)
 
 module.exports = mongoose;
+
+mongoose.connection.on('connected', () => {
+    console.log('Conexión establecida exitosamente');
+  });
+  
+  mongoose.connection.on('error', (err) => {
+    console.error('Error en la conexión:', err);
+  });
+  
+  mongoose.connection.on('disconnected', () => {
+    console.log('La conexión se ha cerrado');
+  });
+  
